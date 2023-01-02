8f5689a4516980c71ee47367321b40662b1432ef
Farhan Nuryadi <farhannuryadi6@gmail.com>
2023-01-03 00:54:33 +0700

    if condition

---
+++ bin/if.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  var nilai = 80;
+  var absen = 80;
+
+  if(nilai >=80 && absen >=75){
+    print("Predikat A");
+  }else if(nilai >= 70 && absen >= 65){
+    print("Predikat B");
+  }else if(nilai >= 60 && absen >= 55){
+    print("Predikat C");
+  }else if(nilai >= 50 && absen >= 45){
+    print("Predikat D");
+  }else{
+    print("Predikat E");
+  }
+
+}
