b41b68b99c08bf39d713241fe4bc0411a46aed7f
Farhan Nuryadi <farhannuryadi6@gmail.com>
2023-01-04 10:39:45 +0700

    for in

---
+++ bin/for_in.dart
@@ -0,0 +1,14 @@
+void main(){
+
+  var fruits = <String>["Apel", "Mangga", "Nanas"];
+
+  for(var fruit in fruits){
+    print(fruit);
+  }
+
+  var numbers = <int>{1,2,3,4,};
+  for(var number in numbers){
+    print(number);
+  }
+
+}
